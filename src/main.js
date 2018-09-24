25454db15feda04a043573b78654c33bd981ddd4
Arthur Manaia <arthurnovais7@gmail.com>
2018-09-23 22:10:15 -0300

    configurando o ambiente com o webpack e webpack-dev-server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-const a = 3;
-const b = 2;
-const c = 'adasdasdasdfsadfsdfasdfsadfasdfsadfasdfasdfsadfasdfsadfasdfsadfsadfsadfasfdasfasdfasdfasdfsadfasdfasfdasdf';
-
-function sum() {
-  return a + b + c;
-}
-
-sum();
+console.log("spotify player")
