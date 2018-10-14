283287a266f16afdb8a041acc6a5a9fc7c8125a0
Arthur Manaia <arthurnovais7@gmail.com>
2018-10-14 17:37:15 -0300

    Criando SelectAlbumTrigger

--- src/searchTrigger.js
+++ src/searchTrigger.js
@@ -5,10 +5,14 @@ const albumList = document.getElementById('album-list');
 const searchInput = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
 
+function makeRequest() {
+  spotify.search.albums(searchInput.value)
+    .then(data => renderAlbums(data.albums.items, albumList));
+}
+
 export default function searchEnterTrigger() {
   searchForm.addEventListener('submit', () => {
     event.preventDefault();
-    spotify.search.albums(searchInput.value)
-      .then(data => renderAlbums(data.albums.items, albumList));
+    makeRequest();
   });
 }
