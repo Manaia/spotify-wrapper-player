48740f8f4b02ba629c7dee0115088e81926a4172
Arthur Manaia <arthurnovais7@gmail.com>
2018-10-14 17:16:31 -0300

    Criando SearchTrigger

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-w';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQCbEcvGP0karft9E45_XaX7IMl1l7QBUtTQ32GIZv9qNDSuTa6tDKioHrsHKvj64Eg_IOiccumtE-HbW-2AZZJevuI0P7VQD6h9I6P1uL1LWvFjwgnbmk0ym0X1944nTx9XJC_zyqbzBdUrgo7q9w',
+  token: 'BQAHSIp3p5eetRQp3XwiAOIVXZ25aum35bcYqS8l5073XM3xj6-UeblieE9xd_AaxwhrSLAPB4uEJv-rgHgJ5alNF56pWPWjN-T-Nm7jZQ7DpBBHsOEN33iaFJk8VRv78pePs-cQHervrr5MUf2wCA',
 });
 
 export default spotify;
---
+++ src/searchTrigger.js
@@ -0,0 +1,14 @@
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albumList = document.getElementById('album-list');
+const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
+
+export default function searchEnterTrigger() {
+  searchForm.addEventListener('submit', () => {
+    event.preventDefault();
+    spotify.search.albums(searchInput.value)
+      .then(data => renderAlbums(data.albums.items, albumList));
+  });
+}
